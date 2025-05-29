316a9e6a565f9337115644fc81f515d5b18e44b7
LeonardoSacra04 <oliveiraleo04@gmail.com>
2025-05-29 00:43:05 -0300

    atualiza o codigo

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int numero, tabuada, tabuadaTotal;
+
+Console.WriteLine("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine()!);
+tabuada = 1;
+
+Console.Clear();
+
+mudaCor();
+Console.WriteLine($"Tabuada do {numero}");
+Console.ResetColor();
+
+while (tabuada <= 10)
+{
+    tabuadaTotal = numero * tabuada;
+    Console.WriteLine($"{numero} x {tabuada} = {tabuadaTotal}");
+    tabuada++;
+}
+
+void mudaCor()
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+}
